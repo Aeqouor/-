81177e292fe08a20188da7dc7fbddd90bbbd67c9
Aeqouor <vitaars@naver.com>
2024-10-31 12:08:04 +0900

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,61 +1,26 @@
-package example.calculator;
-
-import java.util.Scanner;
-
 public class Calculator {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            boolean flag = true;
-            System.out.println("첫번째 숫자를 입력하세요: ");
-            int a = sc.nextInt(); //첫번째 숫자변수
 
-            System.out.println("두번째 숫자를 입력하세요: ");
-            int b = sc.nextInt(); //두번째 숫자 변수
+   private final List<Double> result = new ArrayList<>();
 
-            System.out.println("사칙연산 기호를 입력하세요: ");
-            char sign = sc.next().charAt(0);
-
-            switch (sign) {
-                if resuls = 0;
-                case '+' : System.out.prinln("결과 = "+(a+b)); break;
-                case '-' : System.out.prinln("결과 - "-(a-b)); break;
-                case '*' : System.out.prinln("결과 = "*(a*b)); break;
-                case '/' : Systom.out.prinln("결과 = "/(a/b)); break;
-                if(b = 0) {
-                    System.out.println("0으로 나눌 수 없습니다.")
-                }
-
+    public double calculate(int number1, int number2, char operator) {
+        if (operator == '+') {
+            return number1 + number2;
+        } else if (operator == '-') {
+            return number1 - number2;
+        } else if (operator == '*') {
+            return number1 * number2;
+        } else if (operator == '/') {
+            if (number2 == 0) {
+                System.out.println("0으로는 나눌수 없습니다"); //나누기 예외처리
+            } else {
+                return (double) number1 / number2;
             }
-            switch (sign) {
-                System..out.println("더 계산하시겠습니까?");
-                String exit = sc.next();
-                if(exit.equalsIgnoreCase(  another String "exit")) {
-                    break;
-                }else {
-                    continue;
-                }
-
-
-
-
-            }
+        } else {
+            System.out.println("잘못된 기호를 입력했습니다"); //연산오류처리
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
+        return 0.0;
+    }
 }
+    public List<Double>
+
+
