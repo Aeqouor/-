d3f7d2810b96ad977ec4d700829385a7f11ddc52
Aeqouor <vitaars@naver.com>
2024-11-01 16:19:15 +0900

    Add files via upload
    다시 한거

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Calculator {
+   private List<Double> results = new ArrayList<>();
 
-   private final List<Double> result = new ArrayList<>();
+    public double remove() {
+        double x = results.remove(0);
+        return x;
+    }
 
+    //계산을 수행하는 매서드
     public double calculate(int number1, int number2, char operator) {
         if (operator == '+') {
+            results.add((double) (number1 + number2));
             return number1 + number2;
         } else if (operator == '-') {
+            results.add((double) (number1 - number2));
             return number1 - number2;
         } else if (operator == '*') {
+            results.add((double) (number1 * number2));
             return number1 * number2;
         } else if (operator == '/') {
             if (number2 == 0) {
                 System.out.println("0으로는 나눌수 없습니다"); //나누기 예외처리
             } else {
+                results.add((double) (number1 / number2));
                 return (double) number1 / number2;
             }
         } else {
@@ -20,7 +33,14 @@ public class Calculator {
         }
         return 0.0;
     }
+
+    public List<Double> getResult() {
+        return results;
+    }
+
+    public void setResult(Double result) {
+        this.results.add(result);
+    }
 }
-    public List<Double>
 
 
--- Main.java
+++ Main.java
@@ -1,24 +1,65 @@
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        int number1;
+        int number2;
+        char operator;
 
-        int number1 = sc.nextInt();
-        int number2 = sc.nextInt();
+        String exit = "";
+        while (!exit.equals("exit")){
 
-        char operator = sc.nextInt().charAt(0);
+            System.out.println("첫번째 숫자를 입력하세요");
+            number1 = sc.nextInt();
+            System.out.println("두번째 숫자를 입력하세요");
+            number2 = sc.nextInt();
 
-        if (operator == '+') {
-            number1 + number2
-        } else if (operator == '-') {
-            number1 - number2
-        } else if (operator == '*') {
-            number1 * number2
-        } else if (operator == '/') {
-            number1 / number2
+            //사칙연산 기호
+            System.out.println("사칙연산 기호를 입력하세요");
+            operator = sc.next().charAt(0);
 
+            System.out.println("결과값을 출력");
+
+            double result = calculator.calculate(number1, number2, operator);
+            System.out.println(result);
+
+            System.out.println("종료를 원하면 exit / 더 계산하시겠습니까");
+            sc.nextLine();
+            exit = sc.nextLine();
         }
+
+        System.out.println("가장 먼저 저장된 데이터를 삭제하시겠습니까");
+
+        List<Double> result = calculator.getResult();
+        for (int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
+        calculator.remove();
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
